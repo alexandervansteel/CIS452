91d04ae58cbdfb34e562dfab50c6d6a95cb0ed8d
alexandervansteel <alexandervansteel@gmail.com>
2017-01-24 02:32:45 +0000

    framework for simpleshell

--- Lab02/SimpleShell.c
+++ Lab02/SimpleShell.c
@@ -8,6 +8,7 @@ int main() {
   char line[BUFF_LEN];  // command line
   char* argv[100];      // user command
   char filepath[20];    // full file path
+  char* path = "/bin/"; // set path to bin
   int argc;             // arg count
 
   while (1) {
@@ -19,9 +20,39 @@ int main() {
     size_t len = strlen(line);
     if (line[len - 1] == "\n")
       line[len - 1] = "\0";
-
     if (strcmp(line, "exit") == 0)
       break;
 
+    char *token;
+    token - strtok(line, " ");
+    int i = 0;
+    while (token != NULL) {
+      argv[i] = token;
+      token = strtok(NULL, " ");
+      i++;
+    }
+    argv[i] = NULL;
+
+    argc = i;
+    for (i = 0; i < argc; i++) {
+      printf("%s\n", argc[i]);
+    }
+    strcpy(filepath, path);
+    strcat(filepath, argv[0]);
+
+    for (i = 0; i < strlen(filepath); i++) {
+      if (filepath[i] == '\n')
+        filepath[i] = '\0';
+    }
+
+    int pid = forck();
+
+    if (pid == 0) {
+      execvp( filepath, argv);
+      fprintf(stderr, "Child process could not execvp\n");
+    } else {
+      wait4();
+      printf("Child exited\n");
+    }
   }
 };
