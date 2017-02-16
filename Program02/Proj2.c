e4887444015998dca2613a9606475eb0dd8fbdaa
Alexander Vansteel <alexandervansteel@gmail.com>
2017-02-16 01:00:33 -0500

    program 2 not done. fuck this

--- Program02/Proj2.c
+++ Program02/Proj2.c
@@ -2,22 +2,28 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
 
 //#define MAX 64
 
 void sum(char bin1[], char bin2[], char result[]);
+void exit_program(int);
+
 
 int main(){
   pid_t pid[3];
   int ab[2], bc[2], n;
   pipe(ab), pipe(bc);
-
+  
+  // handle ^C
+  signal(SIGINT, exit_program);
+  
   int MAX;
   printf("Enter the byte size: ");
   scanf("%d", &MAX);
   MAX++;
-
-
+  
+  
   // Child A Complementor
   pid[0] = fork();
   if (pid[0] == 0){
@@ -28,10 +34,10 @@ int main(){
       close(ab[n]);
       close(bc[n]);
     }
-
+    
     FILE* fp;
-    char* buf = (char*)malloc(sizeof(char)*MAX);
-
+    char* buf = malloc(sizeof(char)*MAX);
+    
     if ((fp = fopen("8-input_B.dat", "r")) == NULL){
       perror("ERROR: fopen in Complementor Child\n");
       exit(1);
@@ -40,16 +46,16 @@ int main(){
       if (strlen(buf) == MAX-1){
         strtok(buf, "\n");    // remove trailing \n from fgets
         //fprintf(stderr, "From file in child a: %s\n", buf);
-
+        
         // creates the complement of the binary representation as a string
         char* s = buf;
         while (*s){
           *s++ ^= 1;
         }
-
+        
         //fprintf(stderr, "Sending from child a: %s\n", buf);
-
-        write(STDOUT_FILENO, (const void*)buf, (size_t)strlen(buf)+1);
+        
+        write(STDOUT_FILENO, (const void*)buf, (size_t)strlen(buf));
       }
       memset(buf, '\0', strlen(buf));
     }
@@ -58,7 +64,7 @@ int main(){
     close(ab[1]);
     exit(0);
   }
-
+  
   // Child B Incrementor
   pid[1] = fork();
   if (pid[1] == 0){
@@ -71,17 +77,18 @@ int main(){
       close(ab[n]);
       close(bc[n]);
     }
-
+    
     char* buf = (char*)malloc(sizeof(char)*MAX);
-
+    
     int i;
     while (fgets(buf, MAX, stdin) != NULL){
-      strtok(buf, "\n");    // remove trailing \n from fgets
+      //strtok(buf, "\n");    // remove trailing \n from fgets
       if (strlen(buf) == MAX-1){
-        fprintf(stderr, "Comming into child b: %s\n", buf);
-
+        strtok(buf, "\n");
+        //fprintf(stderr, "Comming into child b: %s\n", buf);
+        
         // adds 1 to make 2s Complement
-        for (i = sizeof(buf); i >= 0; i--){
+        for (i = strlen(buf); i >= 0; i--){
           if (buf[i] == '\0'){  // ignore if NULL character
             continue;
           }
@@ -92,10 +99,10 @@ int main(){
             buf[i] = '0';        // flips any 1 to simulate adding
           }
         }
-
-        fprintf(stderr, "Sending to child c: %s\n", buf);
-
-        write(STDOUT_FILENO, (const void*)buf, (size_t)strlen(buf)+1);
+        
+        //fprintf(stderr, "Sending to child c: %s\n", buf);
+        
+        write(STDOUT_FILENO, (const void*)buf, (size_t)strlen(buf));
       }
       memset(buf, '\0', MAX);
     }
@@ -104,7 +111,7 @@ int main(){
     close(bc[1]);
     exit(0);
   }
-
+  
   // Child C Adder
   pid[2] = fork();
   if (pid[2] == 0){
@@ -115,7 +122,8 @@ int main(){
       close(ab[n]);
       close(bc[n]);
     }
-
+    
+    int carry, temp, j, num1, num2;
     FILE* fp;
     if ((fp = fopen("8-input_A.dat", "r")) == NULL){
       perror("ERROR: fopen in Adder Child\n");
@@ -123,89 +131,78 @@ int main(){
     }
     char* buf = (char*)malloc(sizeof(char)*MAX);
     char* fbuf = (char*)malloc(sizeof(char)*MAX);
-    char* result = (char*)malloc(sizeof(char)*MAX);
-    while ((fgets(fbuf, MAX, fp) != NULL) && (fgets(buf, MAX, stdin) != NULL)){
-
-      strtok(fbuf, "\n");
+    char* line = (char*)malloc(sizeof(char)*MAX + 2);
+    memset(line, '-', MAX+2);
+    //char* result = (char*)malloc(sizeof(char)*MAX);
+    while (fgets(buf, MAX, stdin) != NULL){
+      if (strlen(buf) == MAX-1){
       strtok(buf, "\n");
-
-      sum(buf, fbuf, result);
-      fprintf(stdout, "%s + %s = %s\n", buf, fbuf, result);
-
-      fprintf(stderr, "\nfrom stderr %s + %s = %s\n******\n", buf, fbuf, result);
-
+      fprintf(stderr, "  %s\n", buf);
+      
+      // load number in from vector table
+      while (strlen(fbuf) != MAX){
+        if (fgets(fbuf, MAX+1, fp) == NULL){
+          break;
+        }
+      }
+      strtok(fbuf, "\n");
+      fprintf(stderr, "+ %s\n", fbuf);
+      
+      carry = 0;
+      for(j = MAX -1; j >= 0; j--){
+        // Convert 1 char from each array to int
+        num1 = fbuf[j] - '0';
+        num2 = buf[j] - '0';
+        
+        // Add A + B + Carry
+        temp = num1 + num2 + carry;
+        
+        // if sum is 3 or 2 there is a carry
+        // else there is no carry
+        if (temp == 3 || temp == 2){
+          carry = 1;
+          
+          // temp mod 2 to get the result for this place
+          // 1 when temp is 3, 0 when temp is 2
+          temp %= 2;
+        } else if(temp == 1 || temp == 0){
+          carry = 0;
+        }
+        
+        // convert temp back to the proper char and set result[j]
+        buf[j] = temp + '0';
+      }
+        strtok(buf, "\0");
+      fprintf(stdout, "%s\n  %s\n\n", line, buf);
+    }
       memset(buf, '\0', MAX);
       memset(fbuf, '\0', MAX);
     }
     fclose(fp);
-    free(result);
+    free(line);
     free(fbuf);
     free(buf);
     close(bc[0]);
     exit(0);
   }
-
+  
   // Parent
   // close all other pipes and copies of pipes
   for(n=0; n<2; n++){
     close(ab[n]);
     close(bc[n]);
   }
-
+  
   // report close status
   for (n=0; n<3; n++){
     int status;
     waitpid(pid[n], &status, 0);
-    printf("Child %d exited with status %d\n", n, WEXITSTATUS(status));
+    //printf("Child %d exited with status %d\n", n, WEXITSTATUS(status));
   }
-
+  
   return 0;
 }
 
-void sum(char bin1[], char bin2[], char result[]){
-  int i = strlen(bin1) - 1;
-  int j = strlen(bin2) - 1;
-  int carry = 0, temp, num1, num2;
+void exit_program(sig_num){
 
-  while (i > -1 && j > -1){
-    num1 = bin1[i] - '0';
-    num2 = bin2[j] - '0';
-    temp = num1 + num2 + carry;
-    if (temp / 2 == 1){
-      carry = 1;
-      temp %= 2;
-    }
-    if (i > j){
-      result[i + 1] = temp + '0';
-      result[strlen(bin1) + 1] = '\0';
-    } else {
-      result[j +1] = temp + '0';
-      result[strlen(bin2) + 1] = '\0';
-    }
-    i--;
-    j--;
-  }
-  while (i > -1){
-    temp = bin1[i] + carry - '0';
-    if (temp / 2 == 1){
-      carry = 1;
-      temp %= 2;
-    }
-    result[i + 1] = temp + '0';
-    i--;
-  }
-  while (j > -1){
-    temp = bin2[j] + carry - '0';
-    if (temp / 2 == 1){
-      carry = 1;
-      temp %= 2;
-    }
-    result[j + 1] = temp + '0';
-    j--;
-  }
-  if (carry){
-    result[0] = '1';
-  } else {
-    result[0] = '0';
-  }
 }
