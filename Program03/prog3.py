acb67b277f5ec4a887c70052431863b1c82c54f8
alexandervansteel <alexandervansteel@gmail.com>
2017-04-02 15:28:28 +0000

    trying to figure out how to share the screen

--- Program03/prog3.py
+++ Program03/prog3.py
@@ -3,56 +3,51 @@
 # Whack-a-Mole
 # Python version with Multiprocessing
 
-import multiprocessing as mp
+from multiprocessing import Process, Lock, Value
+from ctypes import c_char_p
 import curses
 
-# def Mole(line,column):
+stdscr=curses.initscr()
 
-    # while True:
-    #     curses.delay_output(hide_dur) # output delay in ms
-    #     stdscr.addstr(line, column, "a")
+def mole(line,column):
+    stdscr.addstr(line,column,'a')
+#     while True:
+#         #curses.delay_output(hide_dur) # output delay in ms
+#         stdscr.addstr(line, column, "a")
 
+if __name__=='__main__':
+    grid_width=int(input('Enter Grid Width: '))
+    grid_height=int(input('Enter Grid Hight: '))
+    max_moles=int(input('Enter Maximum Number of Moles: '))
+    pop_dur=int(input('Enter Pop Duration in ms: '))
+    hide_dur=int(input('Enter Hide Duration in ms: '))
 
-def GameSpace(grid_width,grid_height,max_moles):
-
-    # for line in range (0, grid_height):
-    #     for column in range (0. grid_width):
-    #         #spawn moles
-    #         p=Process(target=Mole,args=(line,column,))
-    #         p.start()
-    #         p.join()
+    #key=Value('c','a')
+    key=' '
+    score=Value('i',0)
+    lock=Lock()
 
     stdscr=curses.initscr()
-    key=''
-
     curses.noecho()
     curses.cbreak()
 
-    int msg=grid_height+1
+    # spawn moles
+    moles=[]
+    for line in range (0, grid_height):
+        for column in range (0, grid_width):
+            p=Process(target=mole,args=(line,column))
+            moles.append(p)
+            p.start()
 
-    int key_msg=grid_height+2
-    stdscr.addstr(msg,0,'Welcome to Whack-a-Mole! (Press esc to quit)')
+    stdscr.addstr(grid_height+1,0,'Welcome to Whack-a-Mole! (Press esc to quit)')
 
     while key != 27: # esc key
         key=stdscr.getch()
-        stdscr.addstr(key_msg,0,'key pressed %c' % key)
-        stdscr.refresh()
+        stdscr.addstr(grid_height+2,0,'key pressed %c' % key)
+
+    # closes processes
+    for m in moles: m.join()
 
     curses.echo()
     curses.nocbreak()
     curses.endwin()
-
-if __name__=='__main__':
-    grid_width=input('Enter Grid Width: ')
-    grid_height=input('Enter Grid Hight: ')
-    max_moles=input('Enter Maximum Number of Moles: ')
-    pop_dur=input('Enter Pop Duration in ms: ')
-    hide_dur=input('Enter Hide Duration in ms: ')
-
-    # grid_width=(sys.argv[1])
-    # grid_height=(sys.argv[2])
-    # max_moles=(sys.argv[3])
-    # global pop_dur=(sys.argv[4])
-    # global hide_dur=(sys.argv[5])
-
-    GameSpace(grid_width,grid_height,max_moles)
