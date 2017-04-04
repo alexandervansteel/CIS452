ade5b9d1f4154ec631fc92868e687f5c3ce28aad
Alexander Vansteel <alexandervansteel@gmail.com>
2017-04-03 20:48:55 -0400

    score working. everything looks to be in order

--- Program03/prog3.py
+++ Program03/prog3.py
@@ -5,7 +5,9 @@
 
 import curses, random, signal, string, sys, time, threading
 
+
 SIGINT=1
+
 
 def mole_t(t_num,pop_dur,hide_dur,grid_w,grid_h):
     global semaphore
@@ -16,29 +18,26 @@ def mole_t(t_num,pop_dur,hide_dur,grid_w,grid_h):
         time.sleep(random.randrange(0,hide_dur))
         semaphore.acquire()
         char=random.choice(string.ascii_lowercase)
-        y=random.randrange(0,grid_h*2)
-        x=random.randrange(0,grid_w*2)
+        y=random.randrange(0,grid_h)
+        x=random.randrange(0,grid_w)
         screen.addstr(y,x,char)
-        # thread_list[t_num]=char          #TODO: Out of Bound Exception
+        thread_list[t_num]=char
         time.sleep(random.randrange(pop_dur))
         screen.addstr(y,x,' ')
         try:
-            thread_list.remove(char)
+            thread_list=[i.replace(key,' ') for i in thread_list]
         except:
             pass
         semaphore.release()
 
     print('Thread '+str(t_num+1)+' ending')
 
+
 def board_refresh():
     while SIGINT:
         screen.refresh()
         time.sleep(0.01)
 
-
-# def signal_handler(signal,frame):
-#     SIGINT=0
-#     sys.exit()
 
 def main(stdscr,grid_w,grid_h,moles,max_moles,pop_dur,hide_dur):
     global screen
@@ -53,7 +52,7 @@ def main(stdscr,grid_w,grid_h,moles,max_moles,pop_dur,hide_dur):
     key=' '
 
     screen.clear()
-    screen.addstr((2*grid_h)+1,0,'Welcome to Whack-a-Mole! (Press esc to exit)')
+    screen.addstr(grid_h+1,0,'Welcome to Whack-a-Mole! (Press esc to exit)')
     screen.refresh()
     curses.curs_set(0)
 
@@ -63,7 +62,6 @@ def main(stdscr,grid_w,grid_h,moles,max_moles,pop_dur,hide_dur):
     try:
         for t_num in range(moles):
             t=threading.Thread(target=mole_t,args=(t_num,pop_dur,hide_dur,grid_w,grid_h,))
-            thread_list.append(' ')
             t.start()
     except:
         raise Exception("Error: Failed to start threads.")
@@ -74,19 +72,20 @@ def main(stdscr,grid_w,grid_h,moles,max_moles,pop_dur,hide_dur):
     score=0
     while key != 27: # esc char
         key=screen.getch()
-        screen.addstr((2*grid_h)+2,0,'Key Pressed: '+chr(key))
+        screen.addstr(grid_h+2,0,'Key Pressed: '+chr(key))
 
-        if key in thread_list:
+        if chr(key) in thread_list:
             score=score+1
-            screen.addstr((2*grid_h)+3,0,'Moles Hit: '+str(score))
+            screen.addstr((grid_h)+3,0,'Score: '+str(score))
             try:
-                thread_list.remove(key)
+                thread_list=[i.replace(key,' ') for i in thread_list]
             except:
                 pass
 
     global SIGINT
     SIGINT=0
     curses.curs_set(2)
+
 
 if __name__=='__main__':
     grid_w=int(sys.argv[1])
@@ -96,13 +95,10 @@ if __name__=='__main__':
     pop_dur=int(sys.argv[5])
     hide_dur=int(sys.argv[6])
 
-    # if (len(sys.argv) < 7):
-    #     print('Incorrect number of arguments')
-    #     print('Usage: '+sys.argv[0]+' [width] [height] [number of moles] [max moles] [pop duration(ms)] [hide duration(ms)]')
-    #     sys.exit()
-    #
-    # if (grid_w*grid_h) < 3 or (grid_w*grid_h) < 49:
-    #     print('Incorrect grid size: Must be between 3x1 and 7x7')
+    if (len(sys.argv) < 7):
+        print('Incorrect number of arguments')
+        print('Usage: '+sys.argv[0]+' [width] [height] [number of moles] [max moles] [pop duration(ms)] [hide duration(ms)]')
+        sys.exit()
 
     try:
         curses.wrapper(main,grid_w,grid_h,moles,max_moles,pop_dur,hide_dur)
